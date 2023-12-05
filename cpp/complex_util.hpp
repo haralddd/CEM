dbddc215a1aec040a0d37d1ecc0824f632b0afac
haralg <haralbbj@stud.ntnu.no>
2023-12-05 18:22:33 +0100

    Some more experimenting

--- cpp/complex_util.hpp
+++ cpp/complex_util.hpp
@@ -12,19 +12,32 @@ struct AVXComplex {
     __m256d real;
     __m256d imag;
 };
+
 #define MM_CABS(re,im) _mm256_sqrt_pd(_mm256_fmadd_pd(re, re, _mm256_mul_pd(im, im)))
 
-AVXComplex mm_c_sqrt(AVXComplex z) {
-    // Vectorized complex square root
-    // Only works for positive real part, handle other cases separately
-    __m256d r = MM_CABS(z.real, z.imag);
-    __m256d a = _mm256_sqrt_pd(r);
-    __m256d b_re = z.real + r;
-    __m256d b_abs = MM_CABS(b_re, z.imag);
-    __m256d ret_re = a * b_re / b_abs;
-    __m256d ret_im = a * z.imag / b_abs;
 
-    return AVXComplex{ret_re, ret_im};
+static inline AVXComplex mm_c_sqrt_unsafe(AVXComplex z) {
+    // Vectorized principal complex square root
+    // If real part is negative
+    // Imaginary part of A is always positive in our case
+
+    __m256d r = MM_CABS(z.real, z.imag);
+    __m256d half = _mm256_set1_pd(0.5);
+
+    return AVXComplex{
+        _mm256_sqrt_pd(half*(r + z.real)),
+        _mm256_sqrt_pd(half*(r - z.real))
+    };
+}
+
+static inline AVXComplex alpha(__mm256d q, AVXComplex a) {
+    // Vectorized alpha function
+    // alpha(q, a) = sqrt(a - q^2)
+    // Assumes that imag(a) >= 0
+    return mm_c_sqrt_unsafe(AVXComplex{
+        _mm256_fnmadd_pd(q, q, a.real),
+        a.imag
+    });
 }
 
 #endif //CPP_COMPLEX_UTIL_HPP
