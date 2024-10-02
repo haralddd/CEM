3711f108675df43ebb4eee74324460aed1226b52
haralg <harald.sharos@gmail.com>
2024-10-02 11:15:44 +0200

    Verify that precompute code for crystal coincides with glass when there is no anisotropy.

--- Julia/tests/test_solver.jl
+++ Julia/tests/test_solver.jl
@@ -2,8 +2,6 @@ push!(LOAD_PATH, "$(@__DIR__)/../RayleighSolver/")
 using RayleighSolver
 using Plots
 using LinearAlgebra
-using ProfileView
-using BenchmarkTools
 
 function test_reciprocity()
     surf = GaussianSurface(30.0e-9, 100.0e-9)
@@ -93,9 +91,90 @@ function test_solver()
     plot(plt1, plt2, layout=(2, 1), size=(800, 800))
 end
 
-function profile_solver_components()
+function test_crystal_precompute()
+    ε = 2.25 + 1e-4im
+    lambda = 632.8e-9
+    Q = 4
+    Nq = 2048+1
+    ks = [sind(20.0)]
+    L = 10.0e-6
+    Ni = 3
     surf = GaussianSurface(30.0e-9, 100.0e-9)
-    sp, rp = default_params_for_surface_testing(surf)
-    pc = SimPreCompute(rp)
-    solve!(sp, rp, pc)
+
+    rp_isotropic = SimParams(
+        lambda=lambda,
+        Q=Q,
+        Nq=Nq,
+        ks=ks,
+        L=L,
+        Ni=Ni,
+        surf=surf,
+        rescale=true,
+        above=Vacuum(),
+        below=Isotropic(ε, 1.0)
+    )
+    @time pc_i = SimPreCompute(rp_isotropic)
+    validate(pc_i)
+
+    rp_crystal = SimParams(
+        lambda=lambda,
+        Q=Q,
+        Nq=Nq,
+        ks=ks,
+        L=L,
+        Ni=Ni,
+        surf=surf,
+        rescale=true,
+        above=UniaxialCrystal(1.0, 1.0, 1.0, 1.0),
+        below=UniaxialCrystal(ε, ε, 1.0, 1.0)
+    )
+    @time pc_c = SimPreCompute(rp_crystal)
+    validate(pc_c)
+
+
+    ln_Ni = plot()
+    ln_Nc = plot()
+    ln_Ndiff = plot()
+
+    hm_Mi = surface(title="Isotropic")
+    hm_Mc = surface(title="Uniaxial Crystal")
+    hm_Mdiff = surface(title="Error")
+
+    Ni = abs2.(pc_i.Npkn)
+    Nc = abs2.(pc_c.Npkn)
+
+    Mi = log10.(abs2.(pc_i.Mpqn))
+    Mc = log10.(abs2.(pc_c.Mpqn))
+
+    Ndiff = abs2.(pc_i.Npkn .- pc_c.Npkn)
+    Mdiff = abs2.(pc_i.Mpqn .- pc_c.Mpqn)
+
+    for n in axes(pc_i.Mpqn, 3)
+        plot!(ln_Ni, Ni[:,1,n], label = "Isotropic N, n=$n")
+        plot!(ln_Nc, Nc[:,1,n], label = "Uniaxial Crystal N, n=$n")
+        plot!(ln_Ndiff, Ndiff[:,1,n], label = "Error N, n=$n")
+
+        surface!(hm_Mi, Mi[:,:,n] .+ n)
+        surface!(hm_Mc, Mc[:,:,n] .+ n)
+        surface!(hm_Mdiff, Mdiff[:,:,n] .+ n)
+    end
+    
+
+    plot(
+        hm_Mi, ln_Ni,
+        hm_Mc, ln_Nc,
+        hm_Mdiff, ln_Ndiff,
+        layout=(3, 2), size=(800, 800)
+        ) |> display
+    
+    @show maximum(abs2.(pc_i.Mpqn) .- abs2.(pc_c.Mpqn))
+    @show maximum(Mdiff)
+    @show maximum(Mdiff[:,:,1])
+    @show maximum(Mdiff[:,:,2])
+    @show maximum(Mdiff[:,:,3])
+    @show argmax(Mdiff)
+    @show maximum(abs2.(pc_i.Npkn) .- abs2.(pc_c.Npkn))
+    @show maximum(Ndiff)
+    @show argmax(Ndiff)
+    nothing
 end
--- Julia/tests/test_surface.jl
+++ Julia/tests/test_surface.jl
@@ -2,10 +2,6 @@ push!(LOAD_PATH, "$(@__DIR__)/../RayleighSolver/")
 using RayleighSolver
 using Statistics
 using Peaks
-using ProfileView
-using BenchmarkTools
-
-
 
 analytical_slope(::T) where T<:RandomSurface = @error "Analytical slope not implemented for $T"
 analytical_slope(surf::GaussianSurface) = √2 * surf.d / surf.a
@@ -135,24 +131,3 @@ end
 test_gaussian() = test_surf(GaussianSurface(30.0e-9, 100.0e-9))
 test_rect() = test_surf(RectangularSurface(30.0e-9, 0.82, 1.97))
 
-function profile_gaussian_surfacegen()
-    rp, sp = default_params_for_surface_testing(GaussianSurface(30.0e-9, 100.0e-9))
-    function loop_de_loop(sp, rp)
-        for _ in 1:1000
-            generate_surface!(sp, rp)
-        end
-    end
-    @btime generate_surface!($sp, $rp)
-    ProfileView.@profview loop_de_loop(sp, rp)
-end
-
-function profile_rectangular_surfacegen()
-    rp, sp = default_params_for_surface_testing(RectangularSurface(30.0e-9, 0.82, 1.97))
-    function loop_de_loop(sp, rp)
-        for _ in 1:1000
-            generate_surface!(sp, rp)
-        end
-    end
-    @btime generate_surface!($sp, $rp)
-    ProfileView.@profview loop_de_loop(sp, rp)
-end
