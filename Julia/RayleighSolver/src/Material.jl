e9bdb768e089e8530a1a90aedad1327335e21eb9
haralg <harald.sharos@gmail.com>
2024-11-05 16:34:34 +0100

    Some quite large restructuring to calculate both s- and p-type polarization simultaneously

--- Julia/RayleighSolver/src/Material.jl
+++ Julia/RayleighSolver/src/Material.jl
@@ -20,31 +20,3 @@ struct UniaxialCrystal <: Material
     mu_perp::ComplexF64
     mu_para::ComplexF64
 end
-
-function _A(cr::UniaxialCrystal)::ComplexF64
-    return √((cr.mu_para*cr.eps_para)/(cr.mu_perp*cr.eps_perp))
-end
-
-"""
-    alpha(q::Float64, material<:Material)::ComplexF64
-Calculates the perpendicular wave number, alpha ≡ q⟂, based on the parallel wave number `q` and `material` properties.
-
-# Arguments:
-- `q`: Wavenumber - Dimensionless, scaled by ``\\frac{\\omega}{c}``
-- `material`: `Material` containing information about the material
-"""
-function alpha(q::Float64, material::T) where {T<:Material} end
-function alpha(q::Float64, material::Vacuum)::ComplexF64
-    return sqrt(complex(1.0 - q^2))
-end
-function alpha(q::Float64, material::Isotropic)::ComplexF64
-    με = material.eps*material.mu
-    return sqrt(με - q^2)
-end
-function alpha(q::Float64, material::UniaxialCrystal)::ComplexF64
-    A = _A(material)
-    με = material.mu_perp*material.eps_perp
-    return A * sqrt(με - q^2)
-end
-
-function alpha0(q) alpha(q, Vacuum()) end
