23d14f8c45bebb2c2b3aad2ca0de7ddb3e665855
haralg <haralbbj@stud.ntnu.no>
2025-03-21 16:04:55 +0100

    Fixed bugs

--- tests/test_flatsurf.jl
+++ tests/test_flatsurf.jl
@@ -14,6 +14,14 @@ function T(a, a0, κpa)
     return pre * (2a0)
 end
 
+function ema_eps_para(f, eps_m, eps_d)
+    return f * eps_m + (1 - f)*eps_d
+end
+
+function ema_eps_perp(f, eps_m, eps_d)
+    return eps_m*eps_d / ((1-f)*eps_m + f*eps_d)
+end
+
 
 @info "Flat surface test"
 θs = 0.0:1.0:90.0
@@ -21,13 +29,15 @@ surf = FlatSurface()
 above = Vacuum()
 
 # TiO2
-# εpa = 3.62+0.0im
-# εpe = 6.84+0.0im
+eps_para = 3.62+0.0im
+eps_perp = 6.84+0.0im
 
 # Silver and glass layered
-εpa = -17.5+0.48im
-εpe = 2.25+0.0im
-below = Uniaxial(εpe, εpa, 1.0+0.0im, 1.0+0.0im)
+# eps_silver = -17.5+0.48im
+# eps_glass = 2.25+0.0im
+# eps_para = ema_eps_para(0.5, eps_silver, eps_glass)
+# eps_perp = ema_eps_perp(0.5, eps_silver, eps_glass)
+below = Uniaxial(eps_perp, eps_para, 1.0+0.0im, 1.0+0.0im)
 
 data = SolverData(Parameters(surf=surf, θs=θs, above=above, below=below))
 prealloc = Preallocated(data.params)
@@ -37,11 +47,11 @@ solve_single!(prealloc, data)
 
 
 ks = sind.(θs)
-ap = alpha_p.(ks, A(below), εpe)
-as = alpha_s.(ks, εpa)
+ap = alpha_p.(ks, A(below), eps_perp)
+as = alpha_s.(ks, eps_para)
 a0 = alpha0.(ks)
 
-Rp = R.(ap, a0, below.eps_perp)
+Rp = R.(ap, a0, below.eps_para)
 Rs = R.(as, a0, below.mu_para)
 
 plt = plot(θs, abs2.(Rp), label=L"\textrm{Analytical}\ R^p", legendfontsize=12, color=:blue)
@@ -51,8 +61,8 @@ scatter!(θs, abs2.(Rs), label=L"\textrm{Numerical}\ R^s", color=:red, markersiz
 # ylims!(0.0, 1.05)
 xticks!(0:10:90)
 
-# savefig(plt, "plots/fresnel_tio2.pdf")
-savefig(plt, "plots/fresnel_layered_silver_glass.pdf")
+savefig(plt, "plots/fresnel_tio2.pdf")
+# savefig(plt, "plots/fresnel_layered_silver_glass.pdf")
 
 display(plt)
 
