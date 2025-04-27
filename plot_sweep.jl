31ad75040992b2fd47dbeacd353997eba4e4008c
haralg <harald.sharos@gmail.com>
2025-04-27 20:16:28 +0200

    Split bianisotropic.conf into gaussian/rect variants and enhance plot styling

--- plot_sweep.jl
+++ plot_sweep.jl
@@ -1,25 +1,40 @@
 using RayleighSolver
 using CairoMakie
 using LaTeXStrings
+using Colors
+
+alpha = 1.0
+linewidth = 2.0
+markersize = 12
+colors = [(:red, alpha), (:blue, alpha), (:green, alpha), (:orange, alpha)]
 
 metallic_dir = "output/sweep/metallic"
 
 fig = Figure(fontsize=24, size=(800, 600))
 ax = Axis(fig[1, 1],
-    title = "MDRC",
-    xlabel=L"\theta_s [deg]",
-    ylabel=L"\text{MDRC}",
+    xlabel=L"\theta_s\ [^\circ]",
+    ylabel=L"\text{Inc. MDRC}",
     xticks= -90:30:90,
     xgridvisible=false,
     ygridvisible=false
 )
 
-for fname in readdir(metallic_dir)
+for (i, fname) in enumerate(readdir(metallic_dir))
     label = splitext(fname)[1]
     data = load_solver_data(joinpath(metallic_dir, fname))
     mdrc_data = calc_mdrc(data)
     
-    lines!(ax, mdrc_data.θs, mdrc_data.inc_p[:, 1], label=L"a = %$(label)")
+    ln = lines!(ax, 
+        mdrc_data.θs, mdrc_data.inc_p[:, 1], 
+        label=L"\varepsilon_\perp = %$(label) \varepsilon",
+        color=colors[i],
+        linewidth=linewidth)
+
+    ymax, maxidx = findmax(mdrc_data.inc_p[:, 1])
+    sc = scatter!(ax, mdrc_data.θs[maxidx], ymax, color=colors[i], marker=:hline, markersize=markersize)
+
+    translate!(sc, 0, 0, 10000-ymax)
+    translate!(ln, 0, 0, 10000-ymax)
 end
 axislegend(ax)
 save("plots/metallic.pdf", fig)
@@ -28,22 +43,25 @@ hyperbolic1_dir = "output/sweep/hyperbolic_type1"
 
 fig = Figure(fontsize=24, size=(800, 600))
 ax = Axis(fig[1, 1],
-    title = "MDRC",
-    xlabel=L"\theta_s [deg]",
-    ylabel=L"\text{MDRC}",
-    xticks= -90:10:90,
+    xlabel=L"\theta_s\ [^\circ]",
+    ylabel=L"\text{Inc. MDRC}",
+    xticks= -90:30:90,
     xgridvisible=false,
     ygridvisible=false
 )
 
-for fname in readdir(hyperbolic_dir)
+for (i, fname) in enumerate(readdir(hyperbolic1_dir))
     label = splitext(fname)[1]
-    label == "1.0" && continue
-    data = load_solver_data(joinpath(hyperbolic_dir, fname))
+    data = load_solver_data(joinpath(hyperbolic1_dir, fname))
     mdrc_data = calc_mdrc(data)
-    display(data.params)
     
-    lines!(ax, mdrc_data.θs, mdrc_data.inc_p[:, 1], label=L"a = %$(label)")
+    ln = lines!(ax, 
+        mdrc_data.θs, mdrc_data.inc_p[:, 1], 
+        label=L"\varepsilon_\parallel = %$(label) \varepsilon",
+        color=colors[i],
+        linewidth=linewidth)
+    ymax, maxidx = findmax(mdrc_data.inc_p[:, 1])
+    translate!(ln, 0, 0, 10000-ymax)
 end
 axislegend(ax)
 save("plots/hyberbolic_type1.pdf", fig)
@@ -52,22 +70,28 @@ hyperbolic2_dir = "output/sweep/hyperbolic_type2"
 
 fig = Figure(fontsize=24, size=(800, 600))
 ax = Axis(fig[1, 1],
-    title = "MDRC",
-    xlabel=L"\theta_s [deg]",
-    ylabel=L"\text{MDRC}",
-    xticks= -90:10:90,
+    xlabel=L"\theta_s\ [^\circ]",
+    ylabel=L"\text{Inc. MDRC}",
+    xticks= -90:30:90,
     xgridvisible=false,
     ygridvisible=false
 )
 
-for fname in readdir(hyperbolic2_dir)
+for (i, fname) in enumerate(readdir(hyperbolic2_dir))
     label = splitext(fname)[1]
-    label == "1.0" && continue
     data = load_solver_data(joinpath(hyperbolic2_dir, fname))
     mdrc_data = calc_mdrc(data)
-    display(data.params)
     
-    lines!(ax, mdrc_data.θs, mdrc_data.inc_p[:, 1], label=L"a = %$(label)")
+    ln = lines!(ax, 
+        mdrc_data.θs, mdrc_data.inc_p[:, 1], 
+        label=L"\varepsilon_\perp = %$(label) \varepsilon",
+        color=colors[i],
+        linewidth=linewidth)
+
+    ymax, maxidx = findmax(mdrc_data.inc_p[:, 1])
+    sc = scatter!(ax, mdrc_data.θs[maxidx], ymax, color=colors[i], marker=:hline, markersize=markersize)
+    translate!(sc, 0, 0, 10000-ymax)
+    translate!(ln, 0, 0, 10000-ymax)
 end
 axislegend(ax)
 save("plots/hyberbolic_type2.pdf", fig)
